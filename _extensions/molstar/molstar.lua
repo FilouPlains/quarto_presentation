33be1e141cdd9f41257ca4f14717d3ab3ed955e7
Lucas ROUAUD <lucas.rouaud@gmail.com>
2025-06-18 21:44:15 +0200

    Molstar is working now. Need to clean and add documentation ^^.

--- _extensions/molstar/molstar.lua
+++ _extensions/molstar/molstar.lua
@@ -39,9 +39,91 @@
 
 
 function molstar(args, kwargs)
-  local function get(value, default)
-    return (#value > 0) and value or default
+  local function split(value_list)
+    local result = {}
+    local SEPARATOR = ","
+  
+    for value in string.gmatch(value_list, "([^" .. SEPARATOR .. "]+)") do
+      value = (string.gsub(value, "%[", ""))
+      value = (string.gsub(value, "%]", ""))
+      value = (string.gsub(value, "^%s*(.-)%s*$", "%1"))
+
+      table.insert(result, value)
+    end
+  
+    return result
   end
+
+  local function test_size(value_list, key, length)
+    if #value_list ~= length then
+      error(
+        "Excepted 2 parameters for `" ..
+	key ..
+	"`. Got '" ..
+        #value_list ..
+        "'."
+      )
+    end
+  end
+
+  local parameter = ""
+  local viewer = ""
+
+  for key, value in pairs(kwargs) do
+    if not string.find(key, "load") then
+      if value ~= "true" and value ~= "false" then
+        value = "'" .. value .. "'"
+      end
+
+      parameter = parameter .. "\n" .. key .. ": " .. value .. ","
+    else
+      if key == "loadPdb" then
+        viewer = viewer .. "\nviewer.loadPdb('" .. value .. "')"
+      elseif key == "loadAlphaFoldDb" then
+        viewer = viewer .. "\nviewer.loadAlphaFoldDb('" .. value .. "')"
+      elseif key == "loadStructureFromUrl" then
+        local value_list = split(value)
+        test_size(value_list, key, 2)
+
+        viewer = viewer ..
+	         "\nviewer.loadStructureFromUrl('" ..
+		 value_list[1] ..
+		 "', '" ..
+		 value_list[2] ..
+		 "')"
+      elseif key == "loadVolumeFromUrl" then
+        local value_list = split(value)
+        test_size(value_list, key, 3)
+
+        viewer = viewer ..
+	         "\nviewer.loadVolumeFromUrl({url: '" ..
+		 value_list[1] ..
+		 "', format: '" ..
+		 value_list[2] ..
+		 "', isBinary: " ..
+		 value_list [3] ..
+		 ",}, [{type: 'relative', value: 0}])"
+      elseif key == "loadTrajectory" then
+        local value_list = split(value)
+        test_size(value_list, key, 5)
+
+        viewer = viewer .. 
+	         "\nviewer.loadTrajectory({model: {kind: 'model-url', url: '" ..
+		 value_list[1] ..
+		 "', format: '" ..
+		 value_list[2] ..
+		 "',}, coordinates: {kind: 'coordinates-url', url: '" ..
+		 value_list[3] ..
+		 "', format: '" ..
+		 value_list[4] ..
+		 "', isBinary: " ..
+		 value_list[5] ..
+	         ",}})"
+      end
+    end
+  end
+
+  local viewer = viewer .. ";"
 
   quarto.doc.add_html_dependency({
     name = "molstar",
@@ -60,40 +142,15 @@ function molstar(args, kwargs)
     <div id="app-id-%s" class="molstar_app"></div>
 
     <script type="text/javascript">
-        molstar.Viewer.create("app-id-%s", {
-            emdbProvider: '%s',
-            pdbProvider: '%s',
-            layoutIsExpanded: %s,
-            layoutShowControls: %s,
-            layoutShowLeftPanel: %s,
-            layoutShowLog: %s,
-            layoutShowRemoteState: %s,
-            layoutShowSequence: %s,
-            viewportShowAnimation: %s,
-            viewportShowControls: %s,
-            viewportShowExpand: %s,
-            viewportShowSelectionMode: %s,
-            viewportShowSettings: %s,
-        }).then((viewer) => {
-            viewer.loadSnapshotFromUrl((url = "structure.molx"), "molx");
+        molstar.Viewer.create("app-id-%s", {%s
+        }).then((viewer) => {%s
         });
     </script>
   ]],
     id,
     id,
-    get(kwargs["emdbProvider"], "rcsb"),
-    get(kwargs["pdbProvider"], "rcsb"),
-    get(kwargs["layoutIsExpanded"], "true"),
-    get(kwargs["layoutShowControls"], "true"),
-    get(kwargs["layoutShowLeftPanel"], "true"),
-    get(kwargs["layoutShowLog"], "true"),
-    get(kwargs["layoutShowRemoteState"], "true"),
-    get(kwargs["layoutShowSequence"], "true"),
-    get(kwargs["viewportShowAnimation"], "true"),
-    get(kwargs["viewportShowControls"], "true"),
-    get(kwargs["viewportShowExpand"], "true"),
-    get(kwargs["viewportShowSelectionMode"], "true"),
-    get(kwargs["viewportShowSettings"], "true")
+    parameter,
+    viewer
   )
 
   return pandoc.RawBlock("html", html_code)
